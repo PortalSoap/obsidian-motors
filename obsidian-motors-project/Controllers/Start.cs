782e34b0686fb3ab1a87930924b5e50fb425d3b6
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2022-10-25 03:21:42 -0300

    Versão 1.0

---
+++ obsidian-motors-project/Controllers/Start.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace obsidian_motors_project.Controllers
+{
+    public class Start : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Exquisite()
+        {
+            return View();
+        }
+
+        public IActionResult Luxury()
+        {
+            return View();
+        }
+    }
+}
---
+++ obsidian-motors-project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace obsidian_motors_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
